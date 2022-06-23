e4c8f8312566590092360bd12ed6211b95320207
corb1999 <le.corbusier.29845@gmail.com>
2022-06-23 13:28:37 -0400

    finish the output portions of the functionality.

--- mixer_script_2.R
+++ mixer_script_2.R
@@ -47,5 +47,29 @@ rm(df_geo, dfa)
 
 # output the results -------------------------------------------
 
+clockin()
+write_sf(df_outcome, 
+         (getwd() %ps% '/cement/shapefiles_output/' %ps% 
+          'geo_aggregation_shapefiles.shp'))
+clockout()
 
+qp <- function(pltname, pltpath_suffix = NA, plt_inch = 5) {
+  plt_timestamp <- paste(year(Sys.time()), month(Sys.time()),  
+                         day(Sys.time()), hour(Sys.time()), minute(Sys.time()), 
+                         floor(second(Sys.time())), sep = "-")
+  aa <- ifelse(is.na(pltpath_suffix), "", pltpath_suffix)
+  plt_filepath <- paste0(getwd(), aa)
+  plt_name <- paste0("plt_", pltname, "_", plt_timestamp, ".png")
+  ggsave(filename = plt_name, plot = last_plot(), 
+         path = plt_filepath, scale = 1, device = "png", 
+         height = plt_inch, width = plt_inch * 1.61803399, units = "in")}
+
+fun_map1 <- function(arg_df = df_outcome) {
+  plt1 <- arg_df %>% 
+    ggplot() + 
+    geom_sf(aes(fill = geo_grouping), color = 'black') + 
+    theme_minimal() + 
+    theme(legend.position = 'none')
+  return(plt1)
+}
 
