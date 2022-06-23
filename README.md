ca005a7f392ba4595dd8dcd889b07b75e890237a
corb1999 <le.corbusier.29845@gmail.com>
2022-06-23 14:15:31 -0400

    add instructions and formatting to the readme. add more formatting and helpful instructions to the primary control script cement_maker. ready to push this is the mvp it looks workable in this condition to be downloaded as a zip and run locally.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gypsum
-user-friendly script utility to aggregate zip codes in maps using simple features
+*user-friendly R script utility to aggregate zip codes in maps using simple features and a provided grouping*
+
+## Insutructions
+- Download RStudio (and R) and install these packages: tidyverse, lubridate, readxl, tidycensus, sf
+- Copy the entire **gypsum** project folder into a spot on your local computer.  Then open the project within R Studio.
+- Get a US Census API Key (very easy): [Census API Site](https://api.census.gov/data/key_signup.html)
+- Create your mapping of zip-codes to whatever grouping to you have and save it as a table in an excel file.
+  - Make sure the mapping uses only 5-digit version of zip-codes, and make sure this lookup is the first tab of the workbook. No blank rows at the top of the sheet, no blank rows, format it like a simple clean table.
+  - Save a copy of this excel file to this folder in the directory and delete the excel file that is currently there. **(/gypsum/sand/put_only_one_excel_inside_this_folder_here)** 
+  - This is where the program will look for your excel mapping. Look at the excel file that is currently there for a helpful example. Just make sure to delete that example file after you put yours in its place.
+- Now open the cement_maker.R script in R Studio and run/alter the code as it instructs. The aggregation will be performed and output a map and the shapefiles into the **/cement** folder. If you run into trouble, make a note of where the program starts to go wrong, and ask your peers for help!
--- cement_maker.R
+++ cement_maker.R
@@ -1,34 +1,56 @@
 
 # ****Start by putting your excel mapping in the right folder!!!!!
+# ****Make sure it is the only excel file there. Delete any other excel
+#   files in that particular folder or the program will get confused
+
+# ***To run a line of code, put your blinking cursor on that line and hit 
+#   CTRL+ENTER on your keyboard to run it. You can highlight multiple lines
+#   of code at then to CTRL+ENTER to run a whole bunch of code.
+#   Lines that start with a hashtag symbol are comments, nothing
+#   will happen if you run a comment.
 
 # run all the code in this block to get the tool initialized ----------------
 
 # source another script
-sourcerpath <- paste0(getwd(), '/mixer/mixer_script_1.R')
-source(file = sourcerpath)
+source(file = paste0(getwd(), '/mixer/mixer_script_1.R'))
 
 # ^ -----
 
-# now, specify some details ----------------------------------
+# now, specify some details... ----------------------------------
 
 # let's clarify what columns contain the zips and which column
-#   is the grouping of the geographies
+#   is the grouping of the geographies. Replace the text in quotes below
+#   first with the name exactly of the zip code column. Second, 
+#   type the exact name of the grouping you want to see
 colnm_geo_code <- 'FHCF ZIP Code'
 colnm_geo_grp <- 'Rating Region'
 
 # now specify which state you are looking at
 state_for_census_lookup <- 'FL'
 
-# input the census API key
+# input the census API key in the quotations. see the instructions
+#   in the README for how to get a key. This allows you to get 
+#   the geometric shapes of all zip codes. This key can be used to 
+#   get lots of other interesting Census data for other projects too!
+#   Put the census key (a very long string of numbers and letters)
+#   in the quotation marks right below this.
 census_api_key('')
 
-
 # ***when you have specified these things, run them to 
-#   tell the computer what to do next
+#   tell the computer what to do next.
 # ^ -----
 
-# once the specifics are made, run this block to do the work -------
+# once the specifics are made above, run this block to do the work -------
+# ***NOTE: this will take a bit of time, that is expected!
 
+# source another script
+clockin()
+source(file = paste0(getwd(), '/mixer/mixer_script_2.R'))
+clockout()
 
+# this function makes a map, the second function prints a copy to the
+#   /cement folder
+fun_map1()
+qp(pltname = 'geo_aggregated_map', pltpath_suffix = '/cement')
 
 # ^ -----
